b17d53d5f2ced834df239d09530b415cb1f3a537
Jelle <62745439+432258jg@users.noreply.github.com>
2023-11-30 13:40:29 +0100

    Added: FPS counter, splicing back in, gaussian blur, confindence counter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import cv2
 import pytesseract
+from collections import Counter
+import datetime
+
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 cap = cv2.VideoCapture(0)
 # To use video example
@@ -12,33 +16,65 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 768)
 cascade = cv2.CascadeClassifier('haarcascade_russian_plate_number.xml')
 upper_left = (200, 200)
 bottom_right = (800, 500)
-# Creating a loop to capture each frame of the video in the name of Img
+stringList = []
 while True:
-    _, img = cap.read()  # Capture webcam footage
-    rect_img = img[upper_left[1]: bottom_right[1], upper_left[0]: bottom_right[0]]
+    rect_img = None
+    start = datetime.datetime.now()
 
+    _, img = cap.read()
+    # img = cv2.flip(img, 1)
+    # img = cv2.resize(img, (1024, 768))
+
+    # Slice feed and put border around it
+    sliced = img[upper_left[1]: bottom_right[1], upper_left[0]: bottom_right[0]]
     r = cv2.rectangle(img, upper_left, bottom_right, (100, 50, 200), 5)
 
+    # Converting the image to gray scale
+    gray = cv2.cvtColor(sliced, cv2.COLOR_BGR2GRAY)
+    Gaussian = cv2.GaussianBlur(gray, (7, 7), 0)
 
-    # Converting to grey scale
-    gray_img = cv2.cvtColor(rect_img, cv2.COLOR_BGR2GRAY)
+    # Detecting the plate number
+    number_plates = cascade.detectMultiScale(Gaussian, 1.1, 4)
 
-    # Allowing multiple face detection
-    #//plate = cascade.
-    faces = cascade.detectMultiScale(gray_img, 1.1, 20, 1)
 
-    # Creating Rectangle around plates
-    for (x, y, w, h) in faces:
-        cv2.rectangle(rect_img, (x, y), (x + w, y + h), (0, 0, 250), 2)
-        plate = img[(x, y), (x + w, y + h)]
-        plate = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
-        #cv2.imshow('Detected', plate)
-        print(pytesseract.image_to_string(plate))
+    # Drawing a rectangle around the plate number
+    for (x, y, w, h) in number_plates:
+        cv2.rectangle(sliced, (x, y), (x + w, y + h), (255, 0, 0), thickness=3)
+        rect_img = sliced[y: y + h, x: x + w]
+        gray = cv2.cvtColor(rect_img, cv2.COLOR_BGR2GRAY)
+        #Gaussian2 = cv2.GaussianBlur(gray, (7, 7), 0)
+        thr = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                    cv2.THRESH_BINARY_INV, 21, 9)
+        #thresh = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-    #Add sliced part back in
-    img[upper_left[1] : bottom_right[1], upper_left[0] : bottom_right[0]] = rect_img
+        # Extract text via tesseract
+        text = pytesseract.image_to_string(thr, lang='eng')
+        text = text.replace("\n", " ")
 
-    # Displaying the image
+        if text != "":
+            #print("Test :", text)
+            stringList.append(text)
+
+            #wait till 10 samples are extracted
+            if len(stringList) > 8:
+                c = Counter(stringList)
+                test = c.most_common(1)
+                print("Number is :", test[0])
+                # Reset counter
+                stringList = []
+    # Set end time
+    end = datetime.datetime.now()
+    # Calculate the time it took to process one frame
+    fps = f"FPS: {1 / (end - start).total_seconds():.2f}"
+    #if rect_img is not None:
+        # could not broadcast input array from shape (107,323,3) into shape (300,600,3)
+        # img[upper_left[1] : bottom_right[1], upper_left[0] : bottom_right[0]] = rect_img
+        #img[upper_left[1]: upper_left[1] + rect_img.shape[0],
+        #upper_left[0]: upper_left[0] + rect_img.shape[1]] = rect_img
+
+    # Add fps counter in img
+    cv2.putText(img, fps, (50, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 8)
     cv2.imshow('Detected', img)
 
     # Waiting for escape key for image to close adding the break statement to end the face detection screen
